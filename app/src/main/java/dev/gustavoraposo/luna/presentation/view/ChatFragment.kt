ded2b401289e937ddd7defb7e0d20ec9ec37dad8
GustavoRaposo <gustavo_f.raposo@hotmail.com>
2023-05-04 02:50:28 -0300

    feat: chat ui elements

--- app/src/main/java/dev/gustavoraposo/luna/presentation/view/ChatFragment.kt
+++ app/src/main/java/dev/gustavoraposo/luna/presentation/view/ChatFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import dev.gustavoraposo.luna.R
 import dev.gustavoraposo.luna.presentation.viewmodel.ChatViewModel
 
@@ -16,18 +20,34 @@ class ChatFragment : Fragment() {
     }
 
     private lateinit var viewModel: ChatViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var editText: EditText
+    private lateinit var sendButton: Button
+    private lateinit var listenButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        val view = inflater.inflate(R.layout.fragment_chat, container, false)
+        recyclerView = view.findViewById(R.id.recyclerViewChat)
+        editText = view.findViewById(R.id.editText)
+        sendButton = view.findViewById(R.id.buttonSend)
+        listenButton = view.findViewById(R.id.buttonListen)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
-        // TODO: Use the ViewModel
+        sendButton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context, "send", Toast.LENGTH_LONG)
+            // TODO: add message to chat, then send to gpt3
+        })
+        listenButton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context, "listen", Toast.LENGTH_LONG)
+            // TODO: add message to chat, then send to gpt3
+        })
     }
 
 }
